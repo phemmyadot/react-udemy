7fe6dbd72955c79b201322df64321e0f0699ee1c
Babafemi Adojutelegan <Phemmylekan@gmail.com>
2023-03-17 17:38:24 -0500

    demo project

---
+++ src/pages/App.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import './App.css';
+import BooksPage from './BooksPage';
+import PhotoPage from './PhotoPage';
+import { Provider } from "../components/bookApp/bookContext";
+
+
+function App() {
+
+  const apps = [
+    { name: "Photo Page", app: <PhotoPage/> },
+    { name: "Books Page", app: <Provider><BooksPage /></Provider> },
+  ]
+
+
+  const [current, setCurrent] = useState(apps[0]);
+
+  const setApp = (e) => {
+    setCurrent(e)
+  }
+
+  const btns = apps.map((e, i) => <button style={{ background: current.name === e.name ? "green" : "white", color: current.name === e.name ? "white" : "green", cursor: "pointer", border: "1px solid green", padding: "10px", margin: "30px 0" }} key={i} onClick={() => setApp(e)}>{e.name}</button>)
+
+  return <div className='App'>
+    {btns}
+    <hr />
+    {current.app}
+  </div>
+}
+
+export default App;
